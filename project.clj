07b3e2d149cff2c22ed5a1504c422ef1f2493140
Matthew Conway <matthew.f.conway@gmail.com>
2014-08-06 18:51:14 -0400

    lein run -m delaunay.core produces desired output of fourliner

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [;; [org.clojure/clojure "1.6.0"]
                  [com.google.guava/guava "12.0"]]
-  :source-paths ["src/clojure"]
+  :source-paths ["src/clj"]
   :java-source-paths ["src/java"])
--- src/clj/delaunay/core.clj
+++ src/clj/delaunay/core.clj
@@ -1,6 +1,19 @@
-(ns delaunay.core)
+(ns delaunay.core
+  (:import (org.delaunay.algorithm Triangulation Triangulations)
+           (org.delaunay.model Vertex Triangle Edge)
+           (org.delaunay TriangulationDemo)))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn four-liner []
+  (TriangulationDemo/drawTriangulation
+   (doto (Triangulation.)
+     (.addAllVertices (Triangulations/randomVertices 1000 300 300))
+     (.triangulate)
+     )
+   300 300 "triangulation.png"))
+
+
+(defn -main []
+  (four-liner))
+
+(four-liner)
